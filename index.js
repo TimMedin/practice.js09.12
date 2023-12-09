005239517f4a3d06c4e900bc6d153e6d95defb01
TimMedin <timmedin180@gmail.com>
2023-12-09 17:50:00 +0200

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+// Завдання 1
+const input = document.querySelector(".input")
+const btn = document.querySelector(".btn")
+btn.textContent = input
+
+// Завдання 2
+const img = document.querySelector(".image")
+img.src = "https://alexus.com.ua/wp-content/uploads/115.jpg"
+img.alt = "Ronnie"
+console.log(img);
+
+// Завдання 3
+const imgtwo = document.querySelector(".imgtwo")
+const link = document.querySelector(".link")
+link.href = "facebook.com"
+imgtwo.alt = "Insta"
+
+// Завдання 4 
+const list = document.querySelector(".elemnt-list")
+list.firstChild.textContent = "Element text change"
